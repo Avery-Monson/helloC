eff8e1ad1b363a92a0aec9e0c5aacff957f0d597
Avery-Monson <111770720+Avery-Monson@users.noreply.github.com>
2022-08-25 16:41:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Avery Monson
+ *         amonson6@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Avery Monson, Computer Engineering\n");
+
+  return 0;
+}
